aac45cc23238a65841c407e3b77654bb0c3b9f64
Mgr. Vojtěch Skoumal <vojta.skoumal@seznam.cz>
2025-10-17 16:30:57 +0200

    fix: log, abs positioning reverted, default center

--- GUI/PositioningControl.py
+++ GUI/PositioningControl.py
@@ -52,6 +52,7 @@ class PositioningController:
             raise ValueError("Axis must be 'X', 'Y', or 'Z'")
         if feedrate is None:
             feedrate = self.default_simple_move_feedrate
+        print(f'Status before move: {self.grbl_streamer.get_status()}')
         self.set_relative_positioning()
         move_cmd = f"G1 {axis}{distance:.3f} F{feedrate:.3f}"
         print(f"Sending move command: {move_cmd}")
@@ -64,16 +65,11 @@ class PositioningController:
             raise ValueError("Axis must be 'X', 'Y', or 'Z'")
         if feedrate is None:
             feedrate = self.default_simple_move_feedrate
-        # self.set_absolute_positioning()
-        # move_cmd = f"G1 {axis}{position:.3f} F{feedrate:.3f}"
-        # print(f"Sending move command: {move_cmd}")
-        # response = self.grbl_streamer.send_command(move_cmd)
-        # print("Move command response:", response)
-        # self.set_relative_positioning()
-        abs_pos = self.get_absolute_positions()
-        abs_pos = {'X': abs_pos.x, 'Y': abs_pos.y, 'Z': abs_pos.z}
-        distance = position - abs_pos[axis]
-        response = self.simple_move(axis, distance, feedrate)
+        self.set_absolute_positioning()
+        move_cmd = f"G1 {axis}{position:.3f} F{feedrate:.3f}"
+        print(f"Sending move command: {move_cmd}")
+        response = self.grbl_streamer.send_command(move_cmd)
+        print("Move command response:", response)
         return response
 
     def center_stage(self):
